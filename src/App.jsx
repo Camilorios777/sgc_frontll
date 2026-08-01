92bcd4357a81fdb71863db436300d4d980a57de1
Julio Cesar Florez <julioflorezalvarez2804@gmail.com>
2026-08-01 17:11:05 -0500

    Creacion sistema de cursos-Dashboard

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div className='M-4 flex items-center justify-center'>
+       <h1 className="text-5xl text-green-800 text-center mt-4">
+         </h1>Dashboard
+     </div>
+    </>
+  )
+}
+ 
+
+export default App
